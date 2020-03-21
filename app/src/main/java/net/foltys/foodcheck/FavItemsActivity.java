6a284bd50051603333fe7158896fdf6bf92d1b9a
foltysM <michal.foltys0@gmail.com>
2020-03-21 15:58:42 +0100

    Added 2 german & polish, created favItems RecyclerView

---
+++ app/src/main/java/net/foltys/foodcheck/FavItemsActivity.java
@@ -0,0 +1,20 @@
+package net.foltys.foodcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fav_items);
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/net/foltys/foodcheck/FoodProduct.java
+++ app/src/main/java/net/foltys/foodcheck/FoodProduct.java
@@ -3,7 +3,13 @@ package net.foltys.foodcheck;
 public class FoodProduct {
     private int energy_kcal;
     private double fat;
+    private double saturates;
     private double carbohydrates;
-    
+    private double sugar;
+    private double protein;
+    private double salt;
+    private double weight;
+
+
 
 }
