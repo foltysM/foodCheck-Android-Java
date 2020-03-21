fffece7f5a6b521ed4f120838769c584a9ba50ef
foltysM <michal.foltys0@gmail.com>
2020-03-21 13:44:50 +0100

    First Commit (12.03)

---
+++ app/src/main/java/net/foltys/foodcheck/FoodProduct.java
@@ -0,0 +1,9 @@
+package net.foltys.foodcheck;
+
+public class FoodProduct {
+    private int energy_kcal;
+    private double fat;
+    private double carbohydrates;
+    
+
+}
---
+++ app/src/main/java/net/foltys/foodcheck/MainActivity.java
@@ -0,0 +1,14 @@
+package net.foltys.foodcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
